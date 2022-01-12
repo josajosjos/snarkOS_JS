3103ad49360356b199a732bab375cd5e9c6fcd21
josajosjos <92388842+josajosjos@users.noreply.github.com>
2022-01-12 23:07:04 +0700

    Initialize feat/node-nonces

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+// Copyright (C) 2019-2021 Aleo Systems Inc.
+// This file is part of the snarkOS library.
+
+// The snarkOS library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The snarkOS library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
+
+use snarkos::Node;
+
+use anyhow::Result;
+use structopt::StructOpt;
+use tokio::runtime;
+
+fn main() -> Result<()> {
+    // Initialize the runtime configuration.
+    let runtime = runtime::Builder::new_multi_thread()
+        .enable_all()
+        .thread_stack_size(16 * 1024 * 1024)
+        .max_blocking_threads(num_cpus::get().saturating_sub(2).max(1)) // Don't use 100% of the cores
+        .build()?;
+
+    runtime.block_on(Node::from_args().start())?;
+
+    Ok(())
+}
