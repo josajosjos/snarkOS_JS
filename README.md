6496830a5f4e1669b169d54e5b8fa27bfd215931
josajosjos <92388842+josajosjos@users.noreply.github.com>
2022-01-12 23:06:30 +0700

    Initialize wallet

---
+++ README.md
@@ -0,0 +1,57 @@
+<p align="center">
+    <img alt="snarkOS" width="1412" src="https://cdn.aleo.org/snarkos/banner.png">
+</p>
+
+## Development Guide
+
+In one terminal, start the first node by running:
+```
+cargo run --release -- --node 4135 --rpc 3035 --miner aleo1d5hg2z3ma00382pngntdp68e74zv54jdxy249qhaujhks9c72yrs33ddah
+```
+
+After the first node starts, in a second terminal, run:
+```
+cargo run --release
+```
+
+## Usage Guide
+
+### Connecting to Aleo Testnet II
+
+To start a client node, run:
+```
+snarkos
+```
+
+To start a mining node, run:
+```
+snarkos --miner {ALEO_ADDRESS}
+```
+
+To run a node with custom settings, refer to the full list of options and flags available in the CLI.
+
+### Command Line Interface
+
+Full list of CLI flags and options can be viewed with `snarkos --help`:
+
+```
+snarkos
+The Aleo Team <hello@aleo.org>
+
+USAGE:
+    snarkos [FLAGS] [OPTIONS]
+
+FLAGS:
+        --display    If the flag is set, the node will render a read-only display
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+        --miner <miner>              Specify this as a mining node, with the given miner address
+    -n, --network <network>          Specify the network of this node [default: 2]
+        --node <node>                Specify the port for the node server
+        --rpc <rpc>                  Specify the port for the RPC server
+        --username <rpc-username>    Specify the username for the RPC server [default: root]
+        --password <rpc-password>    Specify the password for the RPC server [default: pass]
+        --verbosity <verbosity>      Specify the verbosity of the node [options: 0, 1, 2, 3] [default: 3]
+```
---
+++ src/wallet/wallet.rs
@@ -0,0 +1,69 @@
+// Copyright (C) 2019-2021 Aleo Systems Inc.
+// This file is part of the snarkOS library.
+
+// The snarkOS library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The snarkOS library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
+
+use snarkvm::dpc::prelude::*;
+
+use anyhow::Result;
+use snarkos_ledger::storage::{rocksdb::RocksDB, DataMap, Map, Storage};
+use std::path::PathBuf;
+
+/// A wallet, belonging to a specific address. Currently keeps records of all
+/// coinbases ever mined to this wallet, regardless of whether they are valid
+/// or not on the current chain.
+///
+/// The path leading to the database containing all records will be structured as
+/// {data_path}/{address}/.
+///
+/// The database will just store coinbase transactions under the block hash that
+/// they belong to.
+#[derive(Clone, Debug)]
+pub struct Wallet<N: Network> {
+    /// The address associated with this wallet.
+    address: String,
+    /// A path to a directory containing all coinbase records.
+    data_path: String,
+    /// The RocksDB containing all records for the given address.
+    db: DataMap<N::BlockHash, Transaction<N>>,
+}
+
+impl<N: Network> Wallet<N> {
+    /// Creates a new [`Wallet`].
+    pub fn new(address: String, data_path: String, read_only: bool) -> Result<Self> {
+        let path = PathBuf::from(format!("{}/{}/", data_path, address));
+
+        Ok(Self {
+            address: address.clone(),
+            data_path,
+            db: RocksDB::open(path, 0, read_only)?.open_map(&address)?,
+        })
+    }
+
+    /// Fetch all records from the database.
+    pub fn records(&self) -> Result<Vec<Transaction<N>>> {
+        let mut records = vec![];
+
+        for record in self.db.values() {
+            records.push(record);
+        }
+
+        Ok(records)
+    }
+
+    /// Push a record to the database.
+    pub fn push_record(&self, hash: &N::BlockHash, tx: &Transaction<N>) -> Result<()> {
+        self.db.insert(hash, tx)
+    }
+}
